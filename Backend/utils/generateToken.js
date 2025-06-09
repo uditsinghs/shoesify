d9dcca8edf592109553427122c17d34d9157ad4c
Udit Pratap Singh <153190852+uditsinghs@users.noreply.github.com>
2025-06-09 17:14:31 +0530

    Update generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -5,7 +5,7 @@ export const generateToken = (res, user, message, statusCode) => {
      .cookie("token", token, {
       httpOnly: true,             
       secure: process.env.NODE_ENV === "production", 
-      sameSite: "Strict",        
+      sameSite: "none",        
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     .json({
