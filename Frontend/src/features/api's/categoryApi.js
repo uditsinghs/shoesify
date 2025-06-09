c19d8f55be9dcccd9729cf9afe945f0e1b1b1162
Udit Pratap Singh <153190852+uditsinghs@users.noreply.github.com>
2025-06-09 16:38:07 +0530

    Update categoryApi.js

--- Frontend/src/features/api's/categoryApi.js
+++ Frontend/src/features/api's/categoryApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = "http://localhost:8080/api/v1/category/";
+const BASE_URL = "https://shoesify-backend.onrender.com/api/v1/category/";
 export const categoryApi = createApi({
   reducerPath: "categoryApi",
   baseQuery: fetchBaseQuery({
