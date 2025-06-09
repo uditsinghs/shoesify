9ad169334ad53fe58418e8ea1c0e90d2ae968dd2
Udit Pratap Singh <153190852+uditsinghs@users.noreply.github.com>
2025-06-09 17:44:12 +0530

    Update generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 export const generateToken = (res, user, message, statusCode) => {
   const token = user.generateJWTToken();
   res
     .status(statusCode)
      .cookie("token", token, {
       httpOnly: true,             
-      secure: process.env.NODE_ENV === "production", 
+      secure:process.env.NODE_ENV === "production", 
       sameSite: "none",        
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
